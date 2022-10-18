8d43a44183d6ed7c805d1d78a7cadba4590fe406
mutomakoto <aaakbaru@gmail.com>
2022-10-18 19:17:17 +0900

    Reactの教科書コードを追加

---
+++ react_learning_p38.js
@@ -0,0 +1,26 @@
+// 変数の更新
+const obj1 = {
+    name: '主田',
+    age: 24,
+};
+
+console.log(obj1);
+
+obj1.name = "Nushida";
+console.log(obj1);
+
+// オブジェクトにプロパティの追加
+obj1.address = "Tokyo";
+console.log(obj1);
+
+// 配列を定義
+const arr1 = ["dog", "cat"];
+console.log(arr1);
+
+// 1つ目の値を変更
+arr1[0] = "bird";
+console.log(arr1);
+
+// 値を追加
+arr1.push("monkey");
+console.log(arr1);
